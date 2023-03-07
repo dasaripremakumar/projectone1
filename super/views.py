9ec0da9d1367ec095b9168008bc940447eabb2ae
dasaripremakumar <dasaripremakumarprem@gmail.com>
2023-03-07 17:34:57 +0530

    first commit

---
+++ super/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+def premakumar(request):
+    return HttpResponse('<marquee><h1>dasari premakumar</h1></marquee>')
+def baskar(request):
+    return HttpResponse('<marquee><h1>friend lover loveeeeeeee</h1></maequee>')
